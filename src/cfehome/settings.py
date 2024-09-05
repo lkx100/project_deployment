8e48c30db1f97a187d2e3f9af58de214b712c24b
lkx100 <lk5999950@gmail.com>
2024-09-06 01:02:47 +0530

    added env variables

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ks)9oa96#ivpd4r*=uxd=#k^up-r3yp)(gf23^y_csazu5-qd$"
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DJANGO_DEBUG", cast=bool)
+print("DEBUG = ", DEBUG, " | ", type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app",   # https://saas.prod.railway.app
